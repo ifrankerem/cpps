a02df3959b71e3a3becd616587ac557cbe30cda1
İrfan Kerem Arslan <121255615+ifrankerem@users.noreply.github.com>
2025-10-12 20:36:36 +0000

    added ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,11 +6,16 @@
 /*   By: iarslan <iarslan@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 00:46:27 by iarslan           #+#    #+#             */
-/*   Updated: 2025/10/11 00:46:35 by iarslan          ###   ########.fr       */
+/*   Updated: 2025/10/12 20:31:21 by iarslan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
 int main(void)
 {
+	std::string x = "HI THIS IS BRAIN";
+	
 	
 }
