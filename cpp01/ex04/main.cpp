73cd7a26e7f2abaaeda2b6e368696b769105cd2a
ifrankerem <kaan@DESKTOP-ID8D5VH.localdomain>
2025-10-20 00:53:03 +0300

    ex04 done

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iarslan <iarslan@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 22:16:16 by iarslan           #+#    #+#             */
+/*   Updated: 2025/10/20 00:47:10 by iarslan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <fstream> // for ifstream
+
+int main(int argc, char *argv[])
+{
+	if(argc != 4)
+	{
+		std::cout << "Please use this program as ./sed filename string1 string2" << std::endl;
+		return -1;
+	}
+	std::ifstream input_file(argv[1]);
+	std::string output_name;
+	output_name = output_name + argv[1] + ".replace";
+	std::ofstream output_file(output_name.c_str());
+	if(!input_file.is_open() || !output_file.is_open())
+	{
+		std::cerr << "Couldn't open the file!" << std::endl;
+		output_file.close();
+		input_file.close();
+		return -1;
+	}
+	std::string line;
+	std::string s1 = argv[2];
+	if(s1.empty())
+	{
+		std::cerr << "string 1 cannot be empty!!" << std::endl;
+		return -1;
+	}
+	std::string s2 = argv[3];
+	while(std::getline(input_file,line))
+	{
+		size_t index = line.find(s1);
+		while(index != std::string::npos)
+		{
+			std::string n_line = line.substr(0,index);
+			output_file << n_line;
+			output_file << s2;
+			line = line.substr(index + s1.size());
+			index = line.find(s1);
+		}
+		if(input_file.eof())
+			output_file << line;
+		else
+			output_file << line << std::endl;
+	}
+	input_file.close();
+	output_file.close();
+	return 0;
+}
